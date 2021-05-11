e35543a048a1b1243aaf4a6b0e651ac084b2224d
leesoyun <leesoyun4206@naver.com>
2021-05-11 13:59:56 +0900

    Family_profile

--- app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -11,11 +11,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val profileList = arrayListOf(
-            Profiles(R.drawable.woman,"소윤",22,"알쏭이 친척"),
-            Profiles(R.drawable.woman,"정은",22,"알쏭이 엄마"),
-            Profiles(R.drawable.woman,"수현",22,"알쏭이 언니"),
+            Profiles(R.drawable.alsong,"소윤",22,"알쏭이 언니"),
+            Profiles(R.drawable.alsong,"정은",22,"알쏭이 엄마"),
+            Profiles(R.drawable.alsong,"수현",22,"알쏭이 이모"),
             Profiles(R.drawable.woman,"재은",22,"하늘이 언니"),
-            Profiles(R.drawable.woman,"민진",22,"하늘이 엄마")
+            Profiles(R.drawable.woman,"민진",22,"하늘이 엄마"),
+                Profiles(R.drawable.mari,"현승",26,"마리 아빠")
+
 
         )
 
